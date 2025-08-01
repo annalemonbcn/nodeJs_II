09ac6ca80ad7e58b00fc6e7318e1055ed13253ad
annae <anna.romagosa@smartassets.ai>
2025-08-01 17:26:14 +0200

    getCurrentUser endpoint added

--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -50,4 +50,26 @@ const loginController = (req, res, next) => {
   )(req, res, next);
 };
 
-export { registerController, loginController };
+const getCurrentUserController = async (req, res) => {
+  const { _id } = req.user;
+
+  const user = await userModel
+    .findById(_id)
+    .select("-password -createdAt -updatedAt -__v");
+
+  if (!user) {
+    return res.status(404).json({
+      status: "error",
+      code: 404,
+      message: "User not found",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    code: 200,
+    payload: user,
+  });
+};
+
+export { registerController, loginController, getCurrentUserController };
--- src/routes/sessionsRouter.js
+++ src/routes/sessionsRouter.js
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import "dotenv/config";
 import {
+  getCurrentUserController,
   loginController,
   registerController,
 } from "../controllers/sessions.controller.js";
+import { authenticateJwt } from "../middlewares/index.js";
 
 const router = Router();
 
 router.post("/register", registerController);
 router.post("/login", loginController);
+router.get("/current", authenticateJwt, getCurrentUserController);
 
 export default router;
