826cb18d8cb79b6e286881ed4179c7bc7c41c1b1
annae <anna.romagosa@smartassets.ai>
2025-08-04 13:05:02 +0200

    middlewares refactored

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,38 +1,26 @@
 import passport from "passport";
 
-const authenticateJwt = (req, res, next) => {
-  passport.authenticate("current", { session: false }, async (err, user) => {
-    if (err || !user) {
-      return res.status(401).json({
-        status: "error",
-        code: 401,
-        message: "Unauthorized",
-      });
-    }
-
-    req.user = user;
-
-    next();
-  })(req, res, next);
-};
-
-const authenticateWithCallback = (strategy) => {
+const basePassportAuth = (strategy, onFailure) => {
   return (req, res, next) => {
     passport.authenticate(strategy, { session: false }, (err, user, info) => {
-      if (err) return next(err);
-
-      if (!user) {
-        return res.status(400).json({
-          status: "error",
-          message: info?.message || "Authentication failed",
-        });
-      }
-
+      if (err || !user) return onFailure(req, res, info);
       req.user = user;
       next();
     })(req, res, next);
   };
 };
+
+const authenticateJwt = basePassportAuth("current", (req, res) =>
+  res.status(401).json({ status: "error", code: 401, message: "Unauthorized" })
+);
+
+const authenticateWithCallback = (strategy) =>
+  basePassportAuth(strategy, (req, res, info) =>
+    res.status(400).json({
+      status: "error",
+      message: info?.message || "Authentication failed",
+    })
+  );
 
 const authorizeRoles = (...allowedRoles) => {
   return (req, res, next) => {
